cf47b6806bb190519208394041409ee7073db226
Mian M. Rizwan <promoters98>
2021-05-11 06:02:36 -0400

    set priority for  test cases

--- src/test/java/regression/Base.java
+++ src/test/java/regression/Base.java
@@ -12,7 +12,7 @@ public static WebDriver driver;
 	public static void chromeSetup() {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver.exe");
 		driver=new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
 	
@@ -20,7 +20,7 @@ public static WebDriver driver;
 		driver.get("http://automationpractice.com/index.php");
 	}
 	public static void tearDown() {
-		System.out.println("Closing browser.");
+		System.out.println("*** Closing browser. ***");
 		driver.quit();
 	}
 
