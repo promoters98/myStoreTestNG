01bcbbe97b6f5b7941ba6455ded1b5f8d0a42864
Mian M. Rizwan <promoters98>
2021-05-11 03:25:48 -0400

    TestNG test cases

---
+++ src/test/java/Utils/Data.java
@@ -0,0 +1,9 @@
+package Utils;
+
+public class Data {
+	public String HomePageUrl = "http://automationpractice.com/index.php";
+	public String SigninUrl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+	public String myAccountUrl = "http://automationpractice.com/index.php?controller=my-account";
+	public String username = "test882@test.com";
+	public String password = "123456";
+}
---
+++ src/test/java/regression/Base.java
@@ -0,0 +1,27 @@
+package regression;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	
+public static WebDriver driver;
+	
+	public static void chromeSetup() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+	
+	public static void navigateToHomePage() {
+		driver.get("http://automationpractice.com/index.php");
+	}
+	public static void tearDown() {
+		System.out.println("Closing browser.");
+		driver.quit();
+	}
+
+}
